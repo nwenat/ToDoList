a1c348cac3bd72c9e2d513f6640609f0779bec8c
nwenat <natala.dorozynska@gmail.com>
2022-08-15 10:13:24 +0200

    Seed in DB

--- ToDoList/Models/SeedData.cs
+++ ToDoList/Models/SeedData.cs
@@ -10,12 +10,12 @@ namespace ToDoList.Models
             using (var context = new ToDoListContext(
                 serviceProvider.GetRequiredService<DbContextOptions<ToDoListContext>>()))
             {
-                if (context == null || context.ToDo == null || context.Project == null)
+                if (context == null || context.ToDo == null || context.Project == null || context.Category == null)
                 {
                     throw new ArgumentNullException("Null ToDoListContext");
                 }
 
-                if (context.ToDo.Any() || context.Project.Any())
+                if (context.ToDo.Any() || context.Project.Any() || context.Category.Any())
                 {
                     return;
                 }
@@ -26,29 +26,39 @@ namespace ToDoList.Models
 
                 context.Project.AddRange(p1, p2, p3);
 
+                Category c1 = new Category() { CategoryName = "computer" };
+                Category c2 = new Category() { CategoryName = "in home" };
+                Category c3 = new Category() { CategoryName = "errands" };
+
+                context.Category.AddRange(c1, c2, c3);
+
                 context.ToDo.AddRange(
                     new ToDo
                     {
                         TaskName = "Find intresting companies",
                         Project = p1,
-                        IsUrgent = false
+                        Categories = new[] { c1 },
+                        IsUrgent = false,
                     },
                     new ToDo
                     {
                         TaskName = "Prepare your CV",
                         Project = p1,
+                        Categories = new[] { c1 },
                         IsUrgent = true
                     },
                     new ToDo
                     {
                         TaskName = "New TV",
                         Project = p2,
+                        Categories = new[] { c3 },
                         IsUrgent = false
                     },
                     new ToDo
                     {
                         TaskName = "Find inspiration, sample photos",
                         Project = p3,
+                        Categories = new[] { c1 },
                         IsUrgent = false
                     },
                     new ToDo
