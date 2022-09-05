cdaa2542c35bfbac94e5cd56d324359a2a8bbb3a
nwenat <natala.dorozynska@gmail.com>
2022-09-05 21:27:31 +0200

    navbar in inbox

--- ToDoList/Pages/Inbox.cshtml.cs
+++ ToDoList/Pages/Inbox.cshtml.cs
@@ -11,17 +11,45 @@ namespace ToDoList.Pages
         private SQLToDoRepository sQLToDoRepository;
 
         public IList<ToDo> InboxList { get; set; } = default!;
+        public IList<Project> ProjectsList { get; set; } = default!;
+        public IList<Category> CategoriesList { get; set; } = default!;
+
+        public bool wantProjects = false;
+        public bool wantCategories = false;
 
         public InboxModel(ToDoList.Data.ToDoListContext context)
         {
             _context = context;
             sQLToDoRepository = new SQLToDoRepository(_context);
         }
-        public void OnGet()
+        public void OnGet(int? nav)
         {
             if (_context.ToDo != null)
             {
                 InboxList = (IList<ToDo>)sQLToDoRepository.GetInbox();
+            }
+            if (_context.Project != null)
+            {
+                ProjectsList = (IList<Project>)sQLToDoRepository.GetAllProjects();
+            }
+
+            if (_context.Category != null)
+            {
+                CategoriesList = (IList<Category>)sQLToDoRepository.GetAllCategories();
+            }
+
+            if (nav != null)
+            {
+                if ((int)nav == 1)
+                {
+                    wantProjects = true;
+                    wantCategories = false;
+                }
+                else
+                {
+                    wantProjects = false;
+                    wantCategories = true;
+                }
             }
         }
     }
